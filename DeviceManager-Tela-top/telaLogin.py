00e4269fbaf5265103d4b4a876bce03b6d2892ea
LokiVL <vanessa.lauana.2400@gmail.com>
2023-12-21 15:03:12 -0400

    Add a img na tela de login

--- DeviceManager-Tela-top/telaLogin.py
+++ DeviceManager-Tela-top/telaLogin.py
@@ -2,12 +2,20 @@ from customtkinter import *
 import os
 import requests
 import base64
+from tkinter import *
+from PIL import Image
 telaPrincipal = f'"{os.path.dirname(__file__)}\\DeviceManager-Tela-top\\telaPrincipal.py" '
+Imagens = os.path.dirname(os.path.realpath(__file__))
+img_log = CTkImage(Image.open(Imagens + "/login.png"), size=(500, 600))
 
 winLog = CTk(fg_color="gray")
 winLog.geometry("1000x600")
 winLog.title("MDM - Login")
 winLog.resizable(False, False)
+
+
+img_Label = CTkLabel(winLog, image = img_log, text=None)
+img_Label.place(x = 0, y = 0)
 
 frameLog = CTkFrame(winLog, width=500, height=600, fg_color="black")
 frameWel = CTkFrame(frameLog, width=290, height=300, fg_color="black")
