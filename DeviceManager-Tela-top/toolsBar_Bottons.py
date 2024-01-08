5349effce4dd34ce1f8f99d9106bad5aa9f34e25
LokiVL <vanessa.lauana.2400@gmail.com>
2024-01-08 14:40:02 -0400

    atualização

--- DeviceManager-Tela-top/toolsBar_Bottons.py
+++ DeviceManager-Tela-top/toolsBar_Bottons.py
@@ -17,12 +17,12 @@ def set_carrier(checkedDevices):
     ro_carrier = CTkEntry (base, width=100).place(x=100, y=30)
     def click_carrier():
         for device in checkedDevices:
-            subprocess.call(fastboot_path + f"oem config fsg-id")
-            subprocess.call(fastboot_path + f"erase modemst1")
-            subprocess.call(fastboot_path + f"erase modemst2")
-            subprocess.call(fastboot_path + f"oem config carrier ""%{ro_carrier}%""")
-            subprocess.call(fastboot_path + f"erase modemst1")
-            subprocess.call(fastboot_path + f"erase modemst2")
+            subprocess.call(fastboot_path + f"oem config fsg-id -s {device}")
+            subprocess.call(fastboot_path + f"erase modemst1 -s {device}")
+            subprocess.call(fastboot_path + f"erase modemst2 -s {device}")
+            subprocess.call(fastboot_path + f"oem config carrier "%{ro_carrier}%" -s {device}")
+            subprocess.call(fastboot_path + f"erase modemst1 -s {device}")
+            subprocess.call(fastboot_path + f"erase modemst2 -s {device}")
             finished = CTkLabel (base, text = "Finished", text_color="green").place(x=120, y=130)
 
     button = CTkButton(base, text="Set", width=100, command=click_carrier).place(x=100, y=60)
@@ -35,7 +35,7 @@ def setup_jump(checkedDevices):
         subprocess.call(adb_path + f"-s {device} reboot bootloader")
 
 #change SKU
-def change_sku():
+def change_sku(checkedDevices):
     base = CTkToplevel(fg_color="white") 
     base.geometry("300x200") 
     base.title("Set SKU") 
@@ -43,9 +43,10 @@ def change_sku():
     label = CTkLabel (base, text = "Enter the SKU:", text_color="black").place(x=95, y=3)
     SKU = CTkEntry (base, fg_color="white", text_color="black", width=100).place(x=100, y=30)
     def click_SKU():
-        subprocess.call(fastboot_path + f"oem config sku ""%{SKU}%""")
-        subprocess.call(fastboot_path + f"oem config carrier_sku ""%{SKU}%""")
-        subprocess.call(fastboot_path + f"reboot bootloader")
+        for device in checkedDevices:
+            subprocess.call(fastboot_path + f"oem config sku ""%{SKU}%"" -s {device}")
+            subprocess.call(fastboot_path + f"oem config carrier_sku ""%{SKU}%"" -s {device}")
+            subprocess.call(fastboot_path + f"reboot bootloader -s {device}")
         
         finished = CTkLabel (base, text = "Finished", text_color="green").place(x=120, y=130)
 
@@ -53,30 +54,67 @@ def change_sku():
     base.after(100, base.lift)
 
 #Dual/Single SIM
-def sim_type():
+def sim_type(checkedDevices):
     base = CTkToplevel(fg_color="white") 
     base.geometry("300x200")  
-    base.title("Set SKU")
+    base.title("Set SIM Type")
     base.resizable(False, False) 
     option = IntVar(value=0)
     ss = CTkRadioButton (base, text="Single SIM", text_color="black", variable=option, value=1).place(x=95, y=30)
     ds = CTkRadioButton (base, text="Dual SIM", text_color="black", variable=option, value=2).place(x=95, y=80)
 
     def change():
-        if option == 1:
-            subprocess.call(fastboot_path + f"oem config sku ""%{ss}%""")
-            subprocess.call(fastboot_path + f"oem config carrier_sku ""%{ss}%""")
-            subprocess.call(fastboot_path + f"reboot bootloader")
-            finished = CTkLabel (base, text = "Finished", text_color="green").place(x=120, y=130)
-        elif option == 2:
-            subprocess.call(fastboot_path + f"oem config sku ""%{ds}%""")
-            subprocess.call(fastboot_path + f"oem config carrier_sku ""%{ds}%""")
-            subprocess.call(fastboot_path + f"reboot bootloader")
-            finished = CTkLabel (base, text = "Finished", text_color="green").place(x=120, y=130)
+        for device in checkedDevices:
+            if option == 1:
+                subprocess.call(fastboot_path + f"oem config sku %{ss}% -s {device}")
+                subprocess.call(fastboot_path + f"oem config carrier_sku %{ss}% -s {device}")
+                subprocess.call(fastboot_path + f"reboot bootloader -s {device}")
+                finished = CTkLabel (base, text = "Finished", text_color="green").place(x=120, y=130)
+            elif option == 2:
+                subprocess.call(fastboot_path + f"oem config sku %{ds}% -s {device}")
+                subprocess.call(fastboot_path + f"oem config carrier_sku %{ds}% -s {device}")
+                subprocess.call(fastboot_path + f"reboot bootloader -s {device}")
+                finished = CTkLabel (base, text = "Finished", text_color="green").place(x=120, y=130)
     
 
     button = CTkButton(base, text="Set", width=100, command=change).place(x=95, y=110)
     base.after(100, base.lift)
 
+#E/P-SIM
+def sim_type2(checkedDevices):
+    base = CTkToplevel(fg_color="white") 
+    base.geometry("300x200")  
+    base.title("Set SKU")
+    base.resizable(False, False) 
+    option = IntVar(value=0)
+    ss = CTkRadioButton (base, text="p-SIM", text_color="black", variable=option, value=1).place(x=95, y=30)
+    ds = CTkRadioButton (base, text="e-SIM", text_color="black", variable=option, value=2).place(x=95, y=80)
+
+    def change():
+        for device in checkedDevices:
+            if option == 1:
+                subprocess.call(fastboot_path + f"oem hw esim false -s {device}")
+                subprocess.call(fastboot_path + f"-w -s {device}")
+                subprocess.call(fastboot_path + f"erase userdata -s {device}")
+                subprocess.call(fastboot_path + f"erase cache -s {device}")
+                subprocess.call(fastboot_path + f"erase modemst1 -s {device}")
+                subprocess.call(fastboot_path + f"erase modemst2 -s {device}")
+                subprocess.call(fastboot_path + f"oem fb_mode_clear -s {device}")
+                subprocess.call(fastboot_path + f"oem config bootmode "" -s {device}")
+                finished = CTkLabel (base, text = "Finished", text_color="green").place(x=120, y=130)
+            elif option == 2:
+                subprocess.call(fastboot_path + f"fastboot oem hw esim true -s {device}")
+                subprocess.call(fastboot_path + f"-w -s {device}")
+                subprocess.call(fastboot_path + f"erase userdata -s {device}")
+                subprocess.call(fastboot_path + f"erase cache -s {device}")
+                subprocess.call(fastboot_path + f"erase modemst1 -s {device}")
+                subprocess.call(fastboot_path + f"erase modemst2 -s {device}")
+                subprocess.call(fastboot_path + f"oem fb_mode_clear -s {device}")
+                subprocess.call(fastboot_path + f"oem config bootmode "" -s {device}")
+                finished = CTkLabel (base, text = "Finished", text_color="green").place(x=120, y=130)
+    
+
+    button = CTkButton(base, text="Set", width=100, command=change).place(x=95, y=110)
+    base.after(100, base.lift)
 
     
